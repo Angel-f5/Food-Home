c301222fe1ab88901474a06863a199d1c6107912
estef1207 <estefany.16.santamaria@gmail.com>
2021-03-04 14:53:44 +0100

    arreglo de filtro por categorias

--- js/categorias.js
+++ js/categorias.js
@@ -1,34 +1,31 @@
-let listaCategorias= [ 
+let listaCategorias = [ 
 
-    { nombre: "Desayunos",
+    { nombre: "desayunos",
       img:"./assets/Logos slide/desayuno.svg",
       id: 1,
     },
 
-    { nombre: "Fast Food",
+    { nombre: "fastfood",
       img:"./assets/Logos slide/pizza.svg",
       id: 2,
     },
 
-    { nombre: "Comida",
+    { nombre: 'comidas',
       img:"./assets/Logos slide/comida-sana.svg",
       id: 3,
     },
 
-    { nombre: "Postres",
+    { nombre: "postres",
       img:"./assets/Logos slide/magdalena.svg",
       id: 4,
     },
 
-    { nombre: "Drinks",
+    { nombre: 'bebidas',
       img:"./assets/Logos slide/copa-de-vino.svg",
       id: 5,
     },
 
-    { nombre: "Todos",
-      img:"./assets/Logos slide/cesta-de-regalo.svg",
-      id: 6,
-    }
+
 
 ];
 
@@ -37,12 +34,18 @@ let slider= "";
 
 function pintarCat(){
     for (let i = 0; i < listaCategorias.length; i++ ){
-    slider += `<div class="categoria" id="${listaCategorias[i].id}" data-numero="${listaCategorias[i].id}">
+    slider += `<div class="categoria" onclick="filterByCategory('${listaCategorias[i].nombre}')">
         <a href="#"><img src="${listaCategorias[i].img}">
         <p>${listaCategorias[i].nombre}</p>
         </div>`
     } 
 }
-
 pintarCat();
+slider+= `<div class="categoria" onclick="escribePlatos()">
+<a href="#"><img src="./assets/Logos slide/cesta-de-regalo.svg">
+<p>Todos</p>
+</div>`;
+     
+
+
 document.querySelector(".menu-slide").innerHTML= slider;
