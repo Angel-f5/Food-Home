bf3aefb214d4e9f36837dd940c64594291ac5bc6
estef1207 <estefany.16.santamaria@gmail.com>
2021-03-04 09:47:58 +0100

    eliminacion del display del manu-slide

--- js/filtro.js
+++ js/filtro.js
@@ -19,7 +19,9 @@ for (let i = 0; i < arrayPlatos.length ; i++) {
     </div>
 </div>` 
     }
-    document.querySelector(".platos").innerHTML = filtrodesayunos;    
+    
+    document.querySelector(".platos").innerHTML = filtrodesayunos;  
+    reloadListeners();  
 }}
 
 
@@ -143,22 +145,3 @@ document.getElementById("6").addEventListener("click", escribePlatos);
 function escribePlatos() {
     location.reload();
 }
-//    let escribe = "";
-//     for(let i = 0; i < arrayPlatos.length; i++) {
-//         escribe+= `<div class="styleInfo ${arrayPlatos[i].categoria}"> 
-//         <img src="${arrayPlatos[i].img}"> 
-//         <div class="infoplato">
-//             <p class="precio">${arrayPlatos[i].precio}</p>
-//             <h3 class="nombreplato">${arrayPlatos[i].nombre}</h3>
-//             <p class="descripcion">${arrayPlatos[i].desc}</p>
-//             <div class="contador">
-//                 <button class="menos masmenos">—</button>
-//                 <p class="numero">${arrayPlatos[i].cantidad}</p>
-//                 <button class="mas masmenos">+</button>
-//             </div>
-//         </div>
-//     </div>`
-//     }
-//     document.querySelector(".platos").innerHTML = escribe;
-// }//
-
